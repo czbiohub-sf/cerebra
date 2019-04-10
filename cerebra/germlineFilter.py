77d5b631d766706bd01e8f0d37936d15cf4f082c
Lincoln Harris <lincoln.harris@czbiohub.org>
2019-04-10 16:36:51 -0700

    moved vcf header to a file

--- cerebra/germlineFilter.py
+++ cerebra/germlineFilter.py
@@ -16,25 +16,18 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 def writeVCF(df, outStr_):
 	""" routine for writing VCF files, from an existing dataframe. 
 		essentially just adding in this horrible vcf header. """
-	header = """##fileformat=VCFv4.2
-##FILTER=<ID=LowQual,Description="Low quality">
-##FORMAT=<ID=AD,Number=R,Type=Integer,Description="Allelic depths for the ref and alt alleles in the order listed">
-##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Approximate read depth (reads with MQ=255 or with bad mates are filtered)">
-##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
-##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
-##FORMAT=<ID=PL,Number=G,Type=Integer,Description="Normalized, Phred-scaled likelihoods for genotypes as defined in the VCF specification">
-##GATKCommandLine=<ID=HaplotypeCaller,CommandLine="HaplotypeCaller  --dbsnp /arg/3/0/hg38.vcf --native-pair-hmm-threads 16 --output /return/0 --input /arg/2/0/TH041.bam --reference /arg/0/0/hg38.fa --disable-read-filter MappingQualityReadFilter --disable-read-filter GoodCigarReadFilter --disable-read-filter NotSecondaryAlignmentReadFilter --disable-read-filter MappedReadFilter --disable-read-filter MappingQualityAvailableReadFilter --disable-read-filter NonZeroReferenceLengthAlignmentReadFilter --disable-read-filter NotDuplicateReadFilter --disable-read-filter PassesVendorQualityCheckReadFilter --disable-read-filter WellformedReadFilter  --gvcf-gq-bands 1 --gvcf-gq-bands 2 --gvcf-gq-bands 3 --gvcf-gq-bands 4 --gvcf-gq-bands 5 --gvcf-gq-bands 6 --gvcf-gq-bands 7 --gvcf-gq-bands 8 --gvcf-gq-bands 9 --gvcf-gq-bands 10 --gvcf-gq-bands 11 --gvcf-gq-bands 12 --gvcf-gq-bands 13 --gvcf-gq-bands 14 --gvcf-gq-bands 15 --gvcf-gq-bands 16 --gvcf-gq-bands 17 --gvcf-gq-bands 18 --gvcf-gq-bands 19 --gvcf-gq-bands 20 --gvcf-gq-bands 21 --gvcf-gq-bands 22 --gvcf-gq-bands 23 --gvcf-gq-bands 24 --gvcf-gq-bands 25 --gvcf-gq-bands 26 --gvcf-gq-bands 27 --gvcf-gq-bands 28 --gvcf-gq-bands 29 --gvcf-gq-bands 30 --gvcf-gq-bands 31 --gvcf-gq-bands 32 --gvcf-gq-bands 33 --gvcf-gq-bands 34 --gvcf-gq-bands 35 --gvcf-gq-bands 36 --gvcf-gq-bands 37 --gvcf-gq-bands 38 --gvcf-gq-bands 39 --gvcf-gq-bands 40 --gvcf-gq-bands 41 --gvcf-gq-bands 42 --gvcf-gq-bands 43 --gvcf-gq-bands 44 --gvcf-gq-bands 45 --gvcf-gq-bands 46 --gvcf-gq-bands 47 --gvcf-gq-bands 48 --gvcf-gq-bands 49 --gvcf-gq-bands 50 --gvcf-gq-bands 51 --gvcf-gq-bands 52 --gvcf-gq-bands 53 --gvcf-gq-bands 54 --gvcf-gq-bands 55 --gvcf-gq-bands 56 --gvcf-gq-bands 57 --gvcf-gq-bands 58 --gvcf-gq-bands 59 --gvcf-gq-bands 60 --gvcf-gq-bands 70 --gvcf-gq-bands 80 --gvcf-gq-bands 90 --gvcf-gq-bands 99 --indel-size-to-eliminate-in-ref-model 10 --use-alleles-trigger false --disable-optimizations false --just-determine-active-regions false --dont-genotype false --max-mnp-distance 0 --dont-trim-active-regions false --max-disc-ar-extension 25 --max-gga-ar-extension 300 --padding-around-indels 150 --padding-around-snps 20 --adaptive-pruning false --do-not-recover-dangling-branches false --recover-dangling-heads false --consensus false --kmer-size 10 --kmer-size 25 --dont-increase-kmer-sizes-for-cycles false --allow-non-unique-kmers-in-ref false --num-pruning-samples 1 --min-dangling-branch-length 4 --max-num-haplotypes-in-population 128 --min-pruning 2 --adaptive-pruning-initial-error-rate 0.001 --pruning-lod-threshold 1.0 --max-unpruned-variants 100 --debug-graph-transformations false --kmer-length-for-read-error-correction 25 --min-observations-for-kmer-to-be-solid 20 --likelihood-calculation-engine PairHMM --base-quality-score-threshold 18 --pair-hmm-gap-continuation-penalty 10 --pair-hmm-implementation FASTEST_AVAILABLE --pcr-indel-model CONSERVATIVE --phred-scaled-global-read-mismapping-rate 45 --native-pair-hmm-use-double-precision false --debug false --use-filtered-reads-for-annotations false --bam-writer-type CALLED_HAPLOTYPES --dont-use-soft-clipped-bases false --capture-assembly-failure-bam false --error-correct-reads false --do-not-run-physical-phasing false --min-base-quality-score 10 --smith-waterman JAVA --correct-overlapping-quality false --emit-ref-confidence NONE --use-new-qual-calculator true --use-old-qual-calculator false --annotate-with-num-discovered-alleles false --heterozygosity 0.001 --indel-heterozygosity 1.25E-4 --heterozygosity-stdev 0.01 --standard-min-confidence-threshold-for-calling 30.0 --max-alternate-alleles 6 --max-genotype-count 1024 --sample-ploidy 2 --num-reference-samples-if-no-call 0 --genotyping-mode DISCOVERY --genotype-filtered-alleles false --contamination-fraction-to-filter 0.0 --output-mode EMIT_VARIANTS_ONLY --all-site-pls false --min-assembly-region-size 50 --max-assembly-region-size 300 --assembly-region-padding 100 --max-reads-per-alignment-start 50 --active-probability-threshold 0.002 --max-prob-propagation-distance 50 --interval-set-rule UNION --interval-padding 0 --interval-exclusion-padding 0 --interval-merging-rule ALL --read-validation-stringency SILENT --seconds-between-progress-updates 10.0 --disable-sequence-dictionary-validation false --create-output-bam-index true --create-output-bam-md5 false --create-output-variant-index true --create-output-variant-md5 false --lenient false --add-output-sam-program-record true --add-output-vcf-command-line true --cloud-prefetch-buffer 40 --cloud-index-prefetch-buffer -1 --disable-bam-index-caching false --sites-only-vcf-output false --help false --version false --showHidden false --verbosity INFO --QUIET false --use-jdk-deflater false --use-jdk-inflater false --gcs-max-retries 20 --gcs-project-for-requester-pays  --disable-tool-default-read-filters false --minimum-mapping-quality 20 --disable-tool-default-annotations false --enable-all-annotations false",Version=4.1.0.0,Date="March 4, 2019 6:25:06 PM UTC">
-"""
+	with open('vcfheader.txt', 'r') as f:
+		header = f.read()
 	
-	df['QUAL'] = 60.28
-	df['FILTER'] = '.'
-	df['INFO'] = 'AC=2;AF=1.00;AN=2;DP=7;ExcessHet=3.0103;FS=0.000;MLEAC=1;MLEAF=0.500;MQ=3.00;QD=30.14;SOR=2.303'
+		df['QUAL'] = 60.28
+		df['FILTER'] = '.'
+		df['INFO'] = 'AC=2;AF=1.00;AN=2;DP=7;ExcessHet=3.0103;FS=0.000;MLEAC=1;MLEAF=0.500;MQ=3.00;QD=30.14;SOR=2.303'
 
-	output_VCF = outStr_
-	with open(output_VCF, 'w') as vcf:
-		vcf.write(header)
+		output_VCF = outStr_
+		with open(output_VCF, 'w') as vcf:
+			vcf.write(header)
 
-	df.to_csv(output_VCF, sep="\t", mode='a', index=False)
+		df.to_csv(output_VCF, sep="\t", mode='a', index=False)
 
 
 
@@ -103,31 +96,27 @@ def getUniqueVCF_entries(patient, cell):
 
 def germlineFilter(metadata, sc_vcf, bulk_vcf, outpath):
 	""" driver function. """
-	print(metadata)
-	print(sc_vcf)
-	print(bulk_vcf)
-
 	global patientMetadata
 
 	# read in patient metadata
-	#cmd = 'aws s3 cp ' + metadata + ' .'
-	#os.system(cmd)
+	cmd = 'aws s3 cp ' + metadata + ' .'
+	os.system(cmd)
 	patientMetadata = pd.read_csv('metadata_all_cells_4.10.19.csv')
 
 	# get a list of all the single-cell VCF files
-	#os.system('sudo mkdir scVCF')
-	#os.system('sudo chmod -R 777 scVCF')
-	#cmd = 'aws s3 cp ' + sc_vcf + ' ./scVCF/ --recursive'
-	#os.system(cmd)
+	os.system('sudo mkdir scVCF')
+	os.system('sudo chmod -R 777 scVCF')
+	cmd = 'aws s3 cp ' + sc_vcf + ' ./scVCF/ --recursive'
+	os.system(cmd)
 	cwd = os.getcwd()
 	vcfDir = cwd + '/scVCF/'
 	scVCF_list = os.listdir(vcfDir)
 
 	# get list of bulk VCF files
-	#os.system('sudo mkdir bulkVCF')
-	#os.system('sudo chmod -R 777 bulkVCF/')
-	#cmd = 'aws s3 cp ' + bulk_vcf + ' ./bulkVCF/ --recursive'
-	#os.system(cmd)
+	os.system('sudo mkdir bulkVCF')
+	os.system('sudo chmod -R 777 bulkVCF/')
+	cmd = 'aws s3 cp ' + bulk_vcf + ' ./bulkVCF/ --recursive'
+	os.system(cmd)
 	bulkVCF_dir = cwd + '/bulkVCF/'
 	bulkVCF_list = os.listdir(bulkVCF_dir)
 
