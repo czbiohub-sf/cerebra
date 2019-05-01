5b67aaad01a0a56fe55204800507d6574ad9e5af
Ubuntu <lincoln.harris@czbiohub.org>
2019-05-01 01:39:41 +0000

    tripped up on intermediate?

--- cerebra/get_mutationcounts_table.py
+++ cerebra/get_mutationcounts_table.py
@@ -29,8 +29,8 @@ def get_filenames_test():
 def get_filenames():
 	""" get file names based on specified path """
 	files = []
-	for file in os.listdir(cwd + "scVCF_filtered_all/"):
-		PATH = cwd + 'scVCF_filtered_all/' + file
+	for file in os.listdir(cwd + "scVCF_filtered_subset/"):
+		PATH = cwd + 'scVCF_filtered_subset/' + file
 		files.append(PATH)
 
 	return files
@@ -112,7 +112,7 @@ def get_genecell_mut_counts(f):
 	tup = [] 
 
 	cell = f.replace(cwd, "")
-	cell = cell.replace('scVCF_filtered_all/', "")
+	cell = cell.replace('scVCF_filtered_subset/', "")
 	cell = cell.replace(".vcf", "")
 	
 	df = VCF.dataframe(f)
@@ -234,7 +234,7 @@ def get_mutationcounts_table(nthread, test, wrkdir):
 	if test_bool:
 		intermediate.to_csv(cwd + "test/mutationcounts_table/geneCellMutationCounts_artifical.csv", index=False)	
 	else:
-		intermediate.to_csv(cwd + "geneCellMutationCounts.csv", index=False)
+		intermediate.to_csv(cwd + "geneCellMutationCounts_subset.csv", index=False)
 
 	cmd = 'rm ' + cwd + 'intermediate.csv'
 	os.system(cmd)
