360b100f36eaef0a1af74683a1f81cf6b929ef02
Rohan Vanheusden <rohan.vanheusden@gmail.com>
2019-06-21 15:17:27 -0700

    Improve method documentation

--- cerebra/mutation_counts.py
+++ cerebra/mutation_counts.py
@@ -15,11 +15,8 @@ class MutationCounter():
     def __init__(self, cosmic_df, hg38_df):
         filtered_cosmic_df = self._make_filtered_cosmic_df(cosmic_df)
         
-        print("Building COSMIC genome tree...")
         self._cosmic_genome_tree = GenomeDataframeTree(lambda row: GenomePosition.from_str(str(row["Mutation genome position"])), filtered_cosmic_df)
-        print("Building reference genome tree...")
         self._hg38_genome_tree = GenomeDataframeTree(GenomePosition.from_gtf_record, hg38_df)
-        print("Setup complete.")
     
     def _cosmic_subset_contains_genome_pos(self, genome_pos):
         return self._cosmic_genome_tree.has_overlap(genome_pos)
@@ -29,6 +26,8 @@ class MutationCounter():
     
     gene_name_pattern = re.compile(r"gene_name \"(.+?)\"")
     def _parse_gene_name(self, record):
+        """Parse out a gene name from a GTF record."""
+        
         gene_name_match = self.gene_name_pattern.search(record[8])
 
         if not gene_name_match:
@@ -37,16 +36,15 @@ class MutationCounter():
         return gene_name_match[1]
 
     def _make_filtered_cosmic_df(self, cosmic_df):
-        """ returns the COSMIC database after lung adeno filter """
-        print("Filtering COSMIC database...")
-        filtered_by_primary_site = cosmic_df.index[cosmic_df["Primary site"] == "lung"].tolist()
-        database_filter = cosmic_df.iloc[filtered_by_primary_site]
+        """Return a view of the input `cosmic_df` filtered for
+        `Primary site` == `lung`."""
 
-        return database_filter
+        filtered_db = cosmic_df.loc[cosmic_df["Primary site"] == "lung"]
+        return filtered_db
 
     def _make_mutation_counts_df(self, cell_genemuts_pairs):
-        """ creates the cell/mutation counts table from the raw output that 
-            get_gene_cell_muts_counts provides """
+        """Transforms a list of tuples in the form
+        (cell_name, {gene_name: mutation_count}) into a `DataFrame`."""
 
         cell_names, cell_gene_muts = list(zip(*cell_genemuts_pairs))
         cell_names, cell_gene_muts = list(cell_names), list(cell_gene_muts)
@@ -68,13 +66,11 @@ class MutationCounter():
         # TODO: cell_names index?
         return pd.DataFrame(index=cell_names, data=mutation_data)
 
-    def find_cell_gene_mut_counts(self, filename):
-        """ returns a dictionary mapping a gene name to the number of observed
-            mutations """
+    def find_cell_gene_mut_counts(self, stream=None, path=None):
+        """Create a `dict` mapping gene names to the number of times a mutation
+        was found in that gene. Accepts a `stream` or a `path` as input."""
 
-        # PyVCF documentation claims that it automatically infers compression type
-        # from the file extension.
-        vcf_reader = vcfpy.Reader.from_path(filename)
+        vcf_reader = vcfpy.Reader.from_stream(stream) if stream is not None else vcfpy.Reader.from_path(path)
 
         gene_mutation_counts = {}
 
@@ -90,42 +86,40 @@ class MutationCounter():
             gene_row = self._find_containing_gene_record(genome_pos)
 
             if gene_row is None:
-                #print("gene_name miss")
                 continue
 
             gene_name = self._parse_gene_name(gene_row)
-
-            # gene_name = find_gene_name(genome_pos, hg38_genome_tree)
 
             gene_mutation_counts[gene_name] = gene_mutation_counts.get(gene_name, 0) + 1
 
         return gene_mutation_counts
 
-    def find_mutation_counts(self, filenames, threads=4):
-        """ generate a cell x gene mutation counts table from a set of germline filtered vcfs """
+    def find_mutation_counts(self, paths, processes=4):
+        """Create a `DataFrame` of mutation counts, where the row indices are
+        cell names and the column indices are gene names."""
 
-        def process_cell(filename):
-            cell_name = Path(filename).stem
-            return (cell_name, self.find_cell_gene_mut_counts(filename))
+        def process_cell(path):
+            cell_name = Path(path).stem
+            return (cell_name, self.find_cell_gene_mut_counts(path=path))
         
         cell_genemuts_pairs = None
 
-        if threads > 1:
-            with ProcessPool(threads) as pool:
-                cell_genemuts_pairs = tqdm(pool.imap(process_cell, filenames), total=len(filenames), smoothing=0.1)
+        if processes > 1:
+            with ProcessPool(processes) as pool:
+                cell_genemuts_pairs = tqdm(pool.imap(process_cell, paths), total=len(paths), smoothing=0.1)
         else:
-            cell_genemuts_pairs = map(process_cell, tqdm(filenames))
+            cell_genemuts_pairs = map(process_cell, tqdm(paths))
 
         cell_genemuts_pairs = list(cell_genemuts_pairs)
         return self._make_mutation_counts_df(cell_genemuts_pairs)
 
 @click.command()
-@click.option("--threads", default=4, prompt="number of threads", required=True, type=int)
+@click.option("--processes", default=4, prompt="number of processes to use for computation", required=True, type=int)
 @click.option("--cosmicdb", prompt="path to cosmic db file (.tsv)", required=True)
 @click.option("--refgenome", prompt="path to reference genome (.gtf)", required=True)
 @click.option("--outfile", prompt="path to output file (.csv)", required=True)
 @click.argument("files", required=True, nargs=-1)
-def count_mutations(threads, cosmicdb, refgenome, outfile, files):
+def count_mutations(processes, cosmicdb, refgenome, outfile, files):
     print("Beginning setup (this may take several minutes!)")
 
     print("Loading COSMIC database...")
@@ -133,10 +127,12 @@ def count_mutations(threads, cosmicdb, refgenome, outfile, files):
     print("Loading reference genome...")
     refgenome_df = pd.read_csv(refgenome, delimiter='\t', header=None)
     
+    print("Building genome trees...")
     mutation_counter = MutationCounter(cosmic_df, refgenome_df)
+    print("Setup complete.")
 
     print("Finding mutations...")
-    result_df = mutation_counter.find_mutation_counts(files, threads=threads)
+    result_df = mutation_counter.find_mutation_counts(files, processes=processes)
 
     print("Writing file...")
     result_df.to_csv(outfile)
