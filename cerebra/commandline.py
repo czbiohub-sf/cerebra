835dfc0e8a6f62fd6596207ac8518d1f01628600
Rohan Vanheusden <rohan.vanheusden@gmail.com>
2019-06-26 13:33:41 -0700

    Update mutation counts command

--- cerebra/commandline.py
+++ cerebra/commandline.py
@@ -9,7 +9,7 @@ import click
 from cerebra.hello import hello
 from cerebra.datadump import s3_import
 from cerebra.germline_filter import germline_filter
-from cerebra.get_mutationcounts_table import get_mutationcounts_table
+from cerebra.mutation_counts import count_mutations
 from cerebra.get_specific_mutations import get_specific_mutations
 from cerebra.get_mutationalburden import get_mutationalburden
 from cerebra.generate_summary_tables import generate_summary_tables
@@ -33,7 +33,7 @@ def cli():
 cli.add_command(hello, name='hello')
 cli.add_command(s3_import, name='s3_import')
 cli.add_command(germline_filter, name='germline_filter')
-cli.add_command(get_mutationcounts_table, name='get_mutationcounts_table')
+# cli.add_command(get_mutationcounts_table, name='get_mutationcounts_table')
 cli.add_command(get_specific_mutations, name='get_specific_mutations')
 cli.add_command(get_mutationalburden, name='get_mutationalburden')
 cli.add_command(generate_summary_tables, name='generate_summary_tables')
@@ -41,6 +41,8 @@ cli.add_command(generate_summary_tables_test, name='generate_summary_tables_test
 cli.add_command(fusion_search, name='fusion_search')
 cli.add_command(fusions_x_cell, name='fusions_x_cell')
 cli.add_command(check_coverage, name='check_coverage')
+
+cli.add_command(count_mutations, name="count-mutations")
 
 if __name__ == "__main__":
     cli()
