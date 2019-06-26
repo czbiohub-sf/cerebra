cff2690397b465bba069099027649a31dcf4bf03
Rohan Vanheusden <rohan.vanheusden@gmail.com>
2019-06-26 15:16:58 -0700

    Fix whitespace and make processes argument optional for germline filter

--- cerebra/germline_filter.py
+++ cerebra/germline_filter.py
@@ -1,7 +1,7 @@
 """
 This program takes in two sets of vcfs, single-cell and bulk (peripheral
-blood, ie. germline) and filters out the common variants found in both sc 
-and bulk. Creates a new directory, filteredOut, that contans the filtered vcfs. 
+blood, ie. germline) and filters out the common variants found in both sc
+and bulk. Creates a new directory, filteredOut, that contans the filtered vcfs.
 """
 
 import pandas as pd
@@ -23,9 +23,9 @@ def create_germline_genome_tree(germline_vcf_paths):
 	return GenomeIntervalTree(GenomePosition.from_vcf_record, germline_vcf_records)
 
 def write_filtered_vcf(cell_vcf_path, germline_tree, out_stream):
-	""" do the germline filter, and return a dataframe with only the 
+	""" do the germline filter, and return a dataframe with only the
 		UNIQUE entries for a given cell """
-	
+
 	cell_vcf = vcfpy.Reader.from_path(cell_vcf_path)
 	out_vcf = vcfpy.Writer.from_stream(out_stream, header=cell_vcf.header)
 
@@ -44,7 +44,7 @@ def write_filtered_vcf(cell_vcf_path, germline_tree, out_stream):
 
 """ launch """
 @click.command()
-@click.option("--processes", default=1, prompt="number of processes to use for computation", required=True, type=int)
+@click.option("--processes", default=1, prompt="number of processes to use for computation", type=int)
 @click.option("--germline", "germline_path", prompt="path to germline vcf files directory", required=True)
 @click.option("--cells", "cells_path", prompt="path to cell vcf files directory", required=True)
 @click.option("--metadata", "metadata_path", prompt="path to metadata csv file", required=True)
@@ -52,12 +52,12 @@ def write_filtered_vcf(cell_vcf_path, germline_tree, out_stream):
 def germline_filter(processes, germline_path, cells_path, metadata_path, out_path):
 	""" given a set of single-cell vcfs and bulk-seq vcfs (peripheral blood), this
 		program subtracts out the mutations common to sc- and bulkVCF. """
-	
+
 	germline_path = Path(germline_path)
 	cells_path = Path(cells_path)
 	metadata_path = Path(metadata_path)
 	out_path = Path(out_path)
-	
+
 	# read in patient metadata
 	metadata_df = pd.read_csv(metadata_path)
 
@@ -71,7 +71,7 @@ def germline_filter(processes, germline_path, cells_path, metadata_path, out_pat
 		cell_vcf_paths = [(cells_path / cell_id).with_suffix(".vcf") for cell_id in cell_ids]
 
 		germline_tree = create_germline_genome_tree(germline_wb_vcf_paths)
-		
+
 		for cell_vcf_path in cell_vcf_paths:
 			if not cell_vcf_path.exists():
 				continue
@@ -91,4 +91,4 @@ def germline_filter(processes, germline_path, cells_path, metadata_path, out_pat
                 list(tqdm(pool.imap(process_patient, all_patient_ids), total=len(all_patient_ids), smoothing=0.1))
 	else:
 		map(process_patient, tqdm(all_patient_ids, smoothing=0.1))
-	
+
