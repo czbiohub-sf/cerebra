7d9abbe3369cc478f77e723318e4b8d0b1442256
Rohan Vanheusden <rohan.vanheusden@gmail.com>
2019-08-23 15:42:12 -0700

    Improve mutation_counts cli interface, make COSMIC optional

--- cerebra/mutation_counts.py
+++ cerebra/mutation_counts.py
@@ -10,24 +10,34 @@ from pathos.pools import _ProcessPool as Pool
 
 from .utils import GenomePosition, GenomeIntervalTree
 
-class MutationCounter():
-    def __init__(self, cosmic_df, hg38_df):
-        filtered_cosmic_df = self._make_filtered_cosmic_df(cosmic_df)
 
-        self._cosmic_genome_tree = GenomeIntervalTree(
-            lambda row: GenomePosition.from_str(str(row["Mutation genome position"])),
-            (record for idx, record in filtered_cosmic_df.iterrows()))
-        self._hg38_genome_tree = GenomeIntervalTree(
-            GenomePosition.from_gtf_record,
-            (record for idx, record in hg38_df.iterrows()))
+class MutationCounter():
+    def __init__(self, cosmic_df, annotation_df):
+        if cosmic_df is not None:
+            filtered_cosmic_df = self._make_filtered_cosmic_df(cosmic_df)
+
+            self._cosmic_genome_tree = GenomeIntervalTree(
+                lambda row: GenomePosition.from_str(
+                    str(row["Mutation genome position"])),
+                (row for idx, row in filtered_cosmic_df.iterrows()))
+        else:
+            self._cosmic_genome_tree = None
+
+        self._annotation_genome_tree = GenomeIntervalTree(
+            lambda feat: GenomePosition.from_gtf_record(feat),
+            (row for _, row in annotation_df.iterrows()))
 
     def _cosmic_subset_contains_genome_pos(self, genome_pos):
+        if not self._cosmic_genome_tree:
+            return False
+
         return self._cosmic_genome_tree.has_overlap(genome_pos)
 
     def _find_containing_gene_record(self, genome_pos):
-        return self._hg38_genome_tree.get_best_overlap(genome_pos)
+        return self._annotation_genome_tree.get_best_overlap(genome_pos)
 
     gene_name_pattern = re.compile(r"gene_name \"(.+?)\"")
+
     def _parse_gene_name(self, metadata):
         """Parse out a gene name from a GTF metadata string."""
 
@@ -53,9 +63,14 @@ class MutationCounter():
         cell_names, cell_gene_muts = list(cell_names), list(cell_gene_muts)
 
         # Aggregate all of the gene names into a 2D array
-        cell_gene_names = [list(gene_muts.keys()) for gene_muts in cell_gene_muts]
+        cell_gene_names = [
+            list(gene_muts.keys()) for gene_muts in cell_gene_muts
+        ]
+
         # Flatten them into a set
-        all_gene_names = set([gene_name for sublist in cell_gene_names for gene_name in sublist])
+        all_gene_names = set([
+            gene_name for sublist in cell_gene_names for gene_name in sublist
+        ])
 
         mutation_data = {}
         for gene_name in all_gene_names:
@@ -73,8 +88,10 @@ class MutationCounter():
         """Create a `dict` mapping gene names to the number of times a mutation
         was found in that gene. Accepts a `stream` or a `path` as input."""
 
-        vcf_reader = vcfpy.Reader.from_stream(stream) if stream is not None else vcfpy.Reader.from_path(path)
+        vcf_reader = vcfpy.Reader.from_stream(
+            stream) if stream is not None else vcfpy.Reader.from_path(path)
 
+        # TODO: Defaultdict would be cleaner.
         gene_mutation_counts, filtered_gene_mutation_counts = {}, {}
 
         for record in vcf_reader:
@@ -83,6 +100,7 @@ class MutationCounter():
             # And is it better to filter out positional duplicates or gene name
             # duplicates?
 
+            # TODO: Report all relevant genes, not just "best" one.
             gene_row = self._find_containing_gene_record(genome_pos)
 
             if gene_row is None:
@@ -93,73 +111,100 @@ class MutationCounter():
             if gene_name is None:
                 continue
 
-            gene_mutation_counts[gene_name] = gene_mutation_counts.get(gene_name, 0) + 1
+            gene_mutation_counts[gene_name] = gene_mutation_counts.get(
+                gene_name, 0) + 1
 
             if not self._cosmic_subset_contains_genome_pos(genome_pos):
                 continue
 
-            filtered_gene_mutation_counts[gene_name] = filtered_gene_mutation_counts.get(gene_name, 0) + 1
+            filtered_gene_mutation_counts[
+                gene_name] = filtered_gene_mutation_counts.get(gene_name,
+                                                               0) + 1
 
         return gene_mutation_counts, filtered_gene_mutation_counts
 
-    def find_mutation_counts(self, paths, processes=4):
+    def find_mutation_counts(self, paths, processes=1):
         """Create a `DataFrame` of mutation counts, where the row indices are
         cell names and the column indices are gene names."""
-
         def init_process(mutation_counter):
             global current_process_mutation_counter
             current_process_mutation_counter = mutation_counter
 
         def process_cell(path):
             cell_name = Path(path).stem
-            return (cell_name, current_process_mutation_counter.find_cell_gene_mut_counts(path=path))
+            return (cell_name,
+                    current_process_mutation_counter.find_cell_gene_mut_counts(
+                        path=path))
 
         if processes > 1:
-            with Pool(processes, initializer=init_process, initargs=(self,)) as pool:
-                results = list(tqdm(pool.imap(process_cell, paths), total=len(paths), smoothing=0.01))
+            with Pool(processes, initializer=init_process,
+                      initargs=(self, )) as pool:
+                results = list(
+                    tqdm(pool.imap(process_cell, paths),
+                         total=len(paths),
+                         smoothing=0.01))
         else:
+            init_process(self)
             results = list(map(process_cell, tqdm(paths)))
 
         cell_genemuts_pairs, filtered_cell_genmuts_pairs = [], []
 
         for cell_name, (mutation_counts, filtered_mutation_counts) in results:
             cell_genemuts_pairs.append((cell_name, mutation_counts))
-            filtered_cell_genmuts_pairs.append((cell_name, filtered_mutation_counts))
+            filtered_cell_genmuts_pairs.append(
+                (cell_name, filtered_mutation_counts))
 
         return (
             self._make_mutation_counts_df(cell_genemuts_pairs),
             self._make_mutation_counts_df(filtered_cell_genmuts_pairs),
         )
 
+
 @click.command()
-@click.option("--processes", default=1, prompt="number of processes to use for computation", type=int)
-@click.option("--cosmicdb", prompt="path to cosmic db file (.tsv)", required=True)
-@click.option("--refgenome", prompt="path to reference genome (.gtf)", required=True)
-@click.option("--outfile", prompt="path to output file (.csv)", required=True)
-@click.argument("files", required=True, nargs=-1)
-def count_mutations(processes, cosmicdb, refgenome, outfile, files):
+@click.option("--processes",
+              "num_processes",
+              help="number of processes to use for computation",
+              default=1)
+@click.option("--cosmicdb",
+              "cosmicdb_path",
+              help="optional path to cosmic db file (.tsv)",
+              default=None)
+@click.option("--annotation",
+              "annotation_path",
+              help="path to reference genome (.gtf)",
+              required=True)
+@click.option("--output",
+              "output_path",
+              help="path to output file (.csv)",
+              required=True)
+@click.argument("input_files", required=True, nargs=-1)
+def count_mutations(num_processes, cosmicdb_path, annotation_path, output_path,
+                    input_files):
     print("Beginning setup (this may take several minutes!)")
 
-    print("Loading COSMIC database...")
-    cosmic_df = pd.read_csv(cosmicdb, delimiter='\t')
-    print("Loading reference genome...")
-    refgenome_df = pd.read_csv(refgenome, delimiter='\t', header=None)
+    if cosmicdb_path:
+        print("Loading COSMIC database...")
+        cosmic_df = pd.read_csv(cosmicdb_path, sep='\t')
+    else:
+        cosmic_df = None
+
+    print("Loading genome annotation...")
+    annotation_df = pd.read_csv(annotation_path, sep='\t')
 
     print("Building genome trees...")
-    mutation_counter = MutationCounter(cosmic_df, refgenome_df)
+    mutation_counter = MutationCounter(cosmic_df, annotation_df)
     print("Setup complete.")
 
     print("Finding mutations...")
-    result_df, filtered_result_df = mutation_counter.find_mutation_counts(files, processes=processes)
+    result_df, filtered_result_df = mutation_counter.find_mutation_counts(
+        input_files, processes=num_processes)
 
     print("Writing file...")
-    outfile = Path(outfile)
+    output_path = Path(output_path)
 
-    result_df.to_csv(outfile)
+    result_df.to_csv(output_path)
     filtered_result_df.to_csv(
-        outfile
-            .with_name(outfile.stem + "_filtered")
-            .with_suffix(outfile.suffix)
-    )
+        output_path.with_name(output_path.stem + "_filtered").with_suffix(
+            output_path.suffix))
 
     print("Done!")
