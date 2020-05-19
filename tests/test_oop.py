613dc88bf23ff1b23b37037e225e61035bc3b48b
Lincoln Harris <lincoln.harris@czbiohub.org>
2020-05-19 10:18:19 -0700

    adding test_oop!

---
+++ tests/test_oop.py
@@ -0,0 +1,86 @@
+''' testing, but like, good coding practice!! '''
+
+import shutil 
+from click.testing import CliRunner
+from itertools import compress
+import os
+import click
+import pandas as pd
+import vcfpy
+from pyfaidx import Fasta
+import unittest
+
+from cerebra.find_aa_mutations import find_aa_mutations
+from cerebra.find_aa_mutations import AminoAcidMutationFinder
+from cerebra.utils import *
+
+
+class BigOlTestCase(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		''' set this fucker up '''
+		self.data_path = os.path.abspath(__file__ + '/../' + 'data/test_find_aa_mutations/')
+		self.cosmicdb_path =  self.data_path + '/CosmicGenomeScreensMutantExport.min.tsv'
+		#self.annotation_path = self.data_path + '/hg38-plus.min.gtf'
+		self.annotation_path = self.data_path + '/gencode.v33.greatestHits.annotation.gtf'
+	
+		self.genomefa_path = self.data_path + '/GRCh38_limited.fa.gz'
+		self.cov_bool = 1
+		self.num_processes = 2
+		self.outpath = self.data_path + '/test_out.csv'
+	
+		#self.input_path = '/Users/lincoln.harris/code/cerebra/cerebra/tmp/vcf/'
+		self.input_path = self.data_path + '/vcf/'
+		self.input_paths = [self.input_path + x for x in os.listdir(self.input_path)]
+
+		cosmic_df = None
+		annotation_df = pd.read_csv(self.annotation_path, sep='\t', skiprows=5)
+
+		cmd = 'gunzip ' + self.genomefa_path
+		os.system(cmd)
+		genome_gunzip_path = str(self.genomefa_path).strip('.gz')
+		genome_faidx = Fasta(genome_gunzip_path)
+
+		self.aa_mutation_finder = AminoAcidMutationFinder(cosmic_df, annotation_df, 
+													genome_faidx, self.cov_bool)
+
+
+	def test_out_basic(self):
+		''' lets actually run a test here '''
+		results_df = self.aa_mutation_finder.find_transcript_mutations(paths=self.input_paths,
+															processes=self.num_processes)
+		results_df = results_df.sort_index() # row sort
+		results_df = results_df.sort_index(axis=1) # column sort
+		results_df.to_csv(self.outpath)
+
+		assert True == True
+		assert os.path.isfile(self.outpath)
+
+
+	def test_df(self):
+		''' what we doin here?'''
+		outfile = pd.read_csv(self.outpath, index_col=0)
+
+		expect_index = ['A1', 'A2', 'A3', 'A4', 'A5']
+		assert list(outfile.index) == expect_index
+
+		expect_cols = ['EGFR', 'KRAS']
+		assert list(outfile.columns) == expect_cols
+
+		a1_egfr_str = outfile.loc['A1']['EGFR']
+		assert 'Leu858Arg' in a1_egfr_str
+
+		a1_kras_str = outfile.loc['A1']['KRAS']
+		assert 'Gly12Ser' in a1_kras_str
+		assert 'Gly13Val' in a1_kras_str
+
+		assert True == True
+
+
+	def test_helloworld(self):
+		''' just a helloworld func, bruh '''
+		assert True == True
+
+
+if __name__ == "__main__":
+    unittest.main()
