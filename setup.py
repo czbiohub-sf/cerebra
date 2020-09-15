70c7f4c69ddc5b9a0bca8850f1492acef2db9375
Lincoln Harris <ljharris018@gmail.com>
2020-09-15 15:42:21 -0700

    v1.1.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='cerebra',
-    version='1.1.2',
+    version='1.1.3',
     description="finds mutants in your scRNA-seq experiment",
     long_description=long_description,
     long_description_content_type='text/markdown',
